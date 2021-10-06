2f7513a4867d016e802b50e38b6f080b8954070c
B <bilal.malik2@hva.nl>
2021-10-06 21:38:56 +0200

    x

---
+++ cpp/Tempratuur_sensor/TempS.h
@@ -0,0 +1,14 @@
+//
+// Created by bilal on 6-10-2021.
+//
+
+#ifndef PROJECT_ROW_TempS_H
+#define PROJECT_ROW_TempS_H
+#include <stdio.h>
+#include <wiringPi.h>
+
+#endif //PROJECT_ROW_TempS_H
+
+const int pin = 4;
+int readValue;
+double toCelcius(int value);
