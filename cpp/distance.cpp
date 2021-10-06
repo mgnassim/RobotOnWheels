824a81fe6f8395b2acbc8db50de73a0b637092a2
B <bilal.malik2@hva.nl>
2021-10-06 13:08:25 +0200

    updated some files and code

--- cpp/distance.cpp
+++ cpp/distance.cpp
@@ -10,14 +10,14 @@ using namespace std::chrono; // nanoseconds, system_clock, second
 int main(){
 
     int  fd;
-    fd=wiringPiI2CSetup (0x31) ;//i2c addres
-    unsigned char arr[2]={00,0x51};//register port and command
+    fd=wiringPiI2CSetup (0x70) ;//i2c addres
+    //unsigned char arr[2]={00,0x51};//register port and command
 
     while (true) {
-        wiringPiI2CWriteReg8 (fd,arr[0], 2);
+        wiringPiI2CWriteReg8 (fd,00, 81);
         sleep_for(seconds(1));
-        int data = wiringPiI2CReadReg16 (fd,1);
-        //cout << "gemeten afstand is: " << data << "\n";
+        int data = wiringPiI2CReadReg16 (fd,3);
+        cout << "gemeten afstand is: " << data << "\n";
     }
 
     return 0;
