d86afa5547501fb10daa5dd6c0e993345c0ecb07
Bilal Malik <bilal.malik2@hva.nl>
2022-01-24 23:48:28 +0100

    Update   ultra.cpp putt some comments made it fresh ultra.cp

--- Server/Ultrasone/ultra.cpp
+++ Server/Ultrasone/ultra.cpp
@@ -1,18 +1,33 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-#include <chrono>
-#include <iostream>
-#include <thread>
 #include <unistd.h>
-using namespace std;
+
+/***
+ * Author: Bilal_Malik
+ * Code for SRF02 ultrasonic sensor
+ *The SRF02 does not require any user calibration
+ ***/
+
+     /* SRF02 registers */
+// MPU I2C address
+#define SENS_ADDR 0x70      /* The address of the SRF02 on I2C*/
+
+/*  Registers */
+#define CMD_REG   0   /* Command register */
+#define RNG_LOW_REG   3  /* low byte result register */
+
+/* Commands */
+#define CMD_RRM_CM   81   /*Command for Real Ranging Mode Result in centimeters*/
+
 
 class Ultrasonic {
+    
     public:
         int returnDistance() {
 
-            wiringPiI2CWriteReg8(wiringPiI2CSetup(0x70),00, 81);
-            sleep(1);
-            return wiringPiI2CReadReg8(wiringPiI2CSetup(0x70),3);
+            wiringPiI2CWriteReg8(wiringPiI2CSetup(SENS_ADDR),CMD_REG, CMD_RRM_CM);// write to reg
+            sleep(1); //give the sensor time for measurement
+            return wiringPiI2CReadReg8(wiringPiI2CSetup(SENS_ADDR),RNG_LOW_REG);//Read result from register in cm
             
     }
 };
