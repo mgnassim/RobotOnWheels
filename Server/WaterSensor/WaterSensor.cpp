09b97ed5c97626ce4f0d1552e1a57b1651249fb6
Stefano Igbinosun <stefano.igbinosun@hva.nl>
2022-01-26 18:28:23 +0100

    Update comment WaterSensor.cpp

--- Server/WaterSensor/WaterSensor.cpp
+++ Server/WaterSensor/WaterSensor.cpp
@@ -16,12 +16,12 @@ public:
     {
         if (wiringPiSetup() == -1)//if wiringPi fails print error.
         {
-            fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));//to get a readable output string for the error number
+            fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));//to get a readable output string for the error number.
         }
 
         if (wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED) == -1)//if wiringPisetup parameters fails print error.
         {
-            fprintf(stdout, "wiringPiSPISetup Failed: %s\n", strerror(errno));//to get a readable string for the error number
+            fprintf(stdout, "wiringPiSPISetup Failed: %s\n", strerror(errno));//to get a readable output string for the error number.
         }
 
         pinMode(CS_MCP3208, OUTPUT);//tells processor to define pin as output.
@@ -29,28 +29,28 @@ public:
 
     int read_mcp3208_adc(unsigned char adcChannel)
     {
-        unsigned char buff[3];
+        unsigned char buff[3];//buffer of 8bits with 3 elements size.
         int adcValue = 0;
 
-        buff[0] = 0x06 | ((adcChannel & 0x07) >> 2);
-        buff[1] = ((adcChannel & 0x07) << 6);
-        buff[2] = 0x00;
+        buff[0] = 0x06 | ((adcChannel & 0x07) >> 2);//6 or adcChannel and 7 shifts 2 position right in binary.
+        buff[1] = ((adcChannel & 0x07) << 6);//adcChannel en 7 shifts 6 position left in binary.
+        buff[2] = 0x00;//stores zero in. 
 
-        digitalWrite(CS_MCP3208, 0); //Low : CS Active
+        digitalWrite(CS_MCP3208, 0); //Low : CS Active conversion.
 
         wiringPiSPIDataRW(SPI_CHANNEL, buff, 3);//read/write simultaneous on selected SPI-bus,SPI-data overwrite data in the buffer.
 
-        buff[1] = 0x0F & buff[1];
-        adcValue = (buff[1] << 8) | buff[2];
+        buff[1] = 0x0F & buff[1];//stores 15 and buff 1.
+        adcValue = (buff[1] << 8) | buff[2];//shift 8 posistion left in binary or 0 stores it in adcvalue.
 
-        digitalWrite(CS_MCP3208, 1); //High : CS Inactive
+        digitalWrite(CS_MCP3208, 1); //High : CS Inactive conversion.
         std::cout << "AdcValue: " << adcValue << std::endl;//prints adcvalue of the sensor.
         return adcValue;// give back conversion value of sensor tru adc. 
     }
 
     int returnMoistureValue()
     {
-        int adcValue[8] = {0};// 
+        int adcValue[8] = {0};//array size of 8 elements and store with 0. 
 
         int val;
 
