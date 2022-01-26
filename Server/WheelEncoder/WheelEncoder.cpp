a0a94d2de4ba543d4af1596c1a7674865a721500
Stefano Igbinosun <stefano.igbinosun@hva.nl>
2022-01-26 01:11:14 +0100

    Command WheelEncoder.cpp

--- Server/WheelEncoder/WheelEncoder.cpp
+++ Server/WheelEncoder/WheelEncoder.cpp
@@ -7,20 +7,20 @@
 #include <stdbool.h>
 #include <iostream>
 
-#define encoderOutput 28
-static volatile int globalCounter;
+#define encoderOutput 28// sensor pin 
+static volatile int globalCounter;//varible reserve in memory and keeps changing.
 int rpm, speed, rps;
 
-int interval = 1000;
-long previousMillis = 0;
-long currentMillis = 0;
-double Circumference = 0.2105;
+int interval = 1000; // 1sec 
+long previousMillis = 0;//extended size variables for number storage.
+long currentMillis = 0;//extended size variables for number storage.
+double Circumference = 0.2105;// circumferce of the roverwheel.
 
 //Interrupt service routine:
 void wheelInterrupt(void)
 {
-    ++globalCounter;
-    fflush(stdout);
+    ++globalCounter;// adds up the pulses of the wheelencoder.
+    fflush(stdout);// clear the output buffer, move data to console.
 }
 
 class WheelEncoder
@@ -28,29 +28,29 @@ class WheelEncoder
 public:
     void initRPM()
     {
-        wiringPiSetup();
-        pullUpDnControl(encoderOutput, PUD_DOWN);
+        wiringPiSetup();// initialise the wiringPisetup and functions.
+        pullUpDnControl(encoderOutput, PUD_DOWN);//sets the resister mode on the given pin.
 
         //initialisation
-        wiringPiISR(encoderOutput, INT_EDGE_FALLING, &wheelInterrupt);
-        previousMillis = millis();
+        wiringPiISR(encoderOutput, INT_EDGE_FALLING, &wheelInterrupt);//calls the adres of the function if input goes from 0 to 1 on the sensor pin.
+        previousMillis = millis();// starts the timer.
     }
 
     int readSpeed()
     {
-        currentMillis = millis();
-        if (currentMillis - previousMillis > interval)
+        currentMillis = millis();//starts second timer while having current time of previous timer.
+        if (currentMillis - previousMillis > interval)//remaining time bigger than 1sec
         {
-            previousMillis = currentMillis;
+            previousMillis = currentMillis;//save up the time of current in previousmillis. 
 
-            rpm = (float)(globalCounter * 60 / 20);
-            rps = rpm / 60;
-            speed = Circumference * rps;
+            rpm = (float)(globalCounter * 60 / 20);// rpm caculation
+            rps = rpm / 60;// rps caculation
+            speed = Circumference * rps;//M/s caculation.
 
-            globalCounter = 0;
+            globalCounter = 0;// resets counter. 
         }
 
-        return speed;
+        return speed;// give back the value of M/s caculation.
         usleep(500);
     }
 };
